adba6c89a9e87d7a8a496dd11bf881d91d8dbc4b
SofranSebastian <sofransebastian@yahoo.com>
2021-03-10 22:59:01 +0200

    client_socket.c: -moved the logic to functions -changed the name of the file for the users -added helpful comments

--- client_socket.c
+++ client_socket.c
@@ -13,49 +13,109 @@
 #define MAX 1024
 #define MAX_USERS 4
 
-void verify_user(char* username, char* password){
+void get_data_from_client(int tcp_socket) {
+    char message_to_send[MAX];
+
+    fgets(message_to_send, sizeof(message_to_send)+1, stdin);
+    message_to_send[strlen(message_to_send)-1] = '\0';
+
+    if ( send(tcp_socket, message_to_send, sizeof(message_to_send), 0) == -1 ) {
+        perror("send()");
+    }  
+}
+
+void sign_out_users() {
+    char temp_user[50] = "";
+    char temp_pass[50] = "";
+    int temp_flag;
+    int users_count = 4;
+
+    FILE *fp = fopen("users.txt","r+");
+            
+    while(!feof(fp) && users_count--){
+        fscanf(fp, "%s", temp_user);
+        fscanf(fp, "%s", temp_pass);
+        fscanf(fp, "%d", &temp_flag);
+    
+        fseek(fp, ftell(fp)-1, SEEK_SET );
+        fprintf(fp, "%d", 0);
+    }
+    
+    fprintf(stdout, "Server closed.\n");
+    fclose(fp);
+}
+
+void get_data_from_server(int tcp_socket) {
+    char received_message[MAX];
+
+    int recv_bytes = recv(tcp_socket, received_message, sizeof(received_message), 0);
+    if (recv_bytes == -1) {
+        perror("recv message");
+        return;
+    }
+    else if (recv_bytes == 0) {
+        sign_out_users();
+        close(tcp_socket);
+        exit(EXIT_SUCCESS);
+    }
+    else {
+        fprintf(stdout, "%s\n", received_message);
+    }
+}
+
+void sign_in_user(char *username) {
+    char temp_user[50] = "";
+    char temp_pass[50] = "";
+    int temp_flag;
+    
+    FILE *fp = fopen("users.txt","r+");
+
+    while (!feof(fp)) {
+        fscanf(fp, "%s", temp_user);
+        fscanf(fp, "%s", temp_pass);
+        fscanf(fp, "%d", &temp_flag);
+
+        if (strcmp(username, temp_user) == 0) {
+            fseek(fp, ftell(fp)-1, SEEK_SET);
+            fprintf(fp, "%d", 1);
+            fclose(fp);
+            return;
+        }
+    }
+}
+
+char *verify_user(char* username, char* password){
 
     char temp_user[50] = "";
     char temp_pass[50] = "";
     int temp_flag;
     
-    FILE *fp = fopen("loggedusers.txt","r+");
+    FILE *fp = fopen("users.txt","r+");
     
-    while(!feof(fp)){
+    while (!feof(fp)) {
         fscanf(fp, "%s", temp_user);
         fscanf(fp, "%s", temp_pass);
         fscanf(fp, "%d", &temp_flag);
 
-        if(strcmp(username, temp_user) == 0){
-            if(strcmp(password, temp_pass) == 0){
-                if(temp_flag){
+        if (strcmp(username, temp_user) == 0) {
+            if (strcmp(password, temp_pass) == 0) {
+                if (temp_flag) {
                     fclose(fp);
-                    printf("User %s already logged!\n", username);
+                    printf("User %s is already logged in!\n", username);
                     exit(EXIT_SUCCESS);
-                }else{
-                    rewind(fp);
-                    while(!feof(fp)){
-                        fscanf(fp, "%s", temp_user);
-                        fscanf(fp, "%s", temp_pass);
-                        fscanf(fp, "%d", &temp_flag);
-                        if(strcmp(username, temp_user) == 0){
-                            fseek(fp, ftell(fp)-1, SEEK_SET );
-                            fprintf(fp, "%d", 1);
-                            fclose(fp);
-                            return;
-                        }
-                    }
                 }
-            }else{
+                else {
+                    return username;
+                }
+            }
+            else {
                 printf("Password is wrong!\n");
                 exit(EXIT_FAILURE);
             }
         }
     }
-
-    printf("User doesn't exist!\n");
+    printf("User %s doesn't exist!\n",username);
     exit(EXIT_FAILURE);
-    
 }
 
 int main(int argc, char* argv[]) {
@@ -64,17 +124,6 @@ int main(int argc, char* argv[]) {
         fprintf(stderr, "Usage: %s username password\n", argv[0]);
         exit(EXIT_FAILURE);
     }
-
-    verify_user(argv[1], argv[2]);
-
-    /* AF_INET -> address familty for the internet protocol v4 */
-    /* SOCK_STREAM -> socket type that open a TCP socket */
-    /* 
-        0 -> a particular protocol to be used with the socket, but usually only
-             a single one exists to support the socket type so we specify it as 0
-
-        socket() returns a file descriptor for the new socket
-    */
 
     int tcp_socket = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -90,15 +139,16 @@ int main(int argc, char* argv[]) {
     server_address.sin_port = SERVER_PORT;
     server_address.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
 
+    char *username = verify_user(argv[1], argv[2]);
+
     int conn = connect(tcp_socket, (struct sockaddr*)&server_address, (socklen_t)sizeof(server_address));
-    
+
     if (conn == -1) {
         perror("connect()");
         exit(EXIT_FAILURE);
     }
-    
-    char message_to_send[MAX];
-    char received_message[MAX];
+
+    sign_in_user(username);
 
     struct pollfd fds[2];
 
@@ -117,49 +167,20 @@ int main(int argc, char* argv[]) {
             perror("poll()");
         }
         
-        if (fds[0].revents & POLLIN) {
-            fgets(message_to_send, sizeof(message_to_send)+1, stdin);
-            message_to_send[strlen(message_to_send)-1] = '\0';
-
-            if ( send(tcp_socket, message_to_send, sizeof(message_to_send), 0) == -1 ) {
-                perror("send()");
-            }  
+        /* 
+            - there is content ready to be read from standard input 
+            - the content (message) will be sent to the server
+        */
+        if (fds[0].revents & POLLIN) { 
+            get_data_from_client(tcp_socket);
         }
-
+        /* 
+            - there is content ready to be read from the server
+            - the content (message) will be sent to the other clients
+        */
         if (fds[1].revents & POLLIN) {
-            int recv_bytes = recv(tcp_socket, received_message, sizeof(received_message), 0);
-            if (recv_bytes == -1) {
-                perror("recv message");
-                continue;
-            }
-            else if (recv_bytes == 0) {
-
-                char temp_user[50] = "";
-                char temp_pass[50] = "";
-                int temp_flag;
-                int users_count = 4;
-
-                FILE *fp = fopen("loggedusers.txt","r+");
-                        
-                while(!feof(fp) && users_count--){
-                    fscanf(fp, "%s", temp_user);
-                    fscanf(fp, "%s", temp_pass);
-                    fscanf(fp, "%d", &temp_flag);
-                
-                    fseek(fp, ftell(fp)-1, SEEK_SET );
-                    fprintf(fp, "%d", 0);
-                }
-                
-                fprintf(stdout, "Server closed.\n");
-                fclose(fp);
-                close(tcp_socket);
-                exit(EXIT_SUCCESS);
-            }
-            else {
-                fprintf(stdout, "%s\n", received_message);
-            }
+            get_data_from_server(tcp_socket);
         }
     }
-    close(tcp_socket);
     return 0;
 }
